0528fe7d46ee8ce945b117df32f9aa44afd33966
mkeresty <kerestymason@gmail.com>
2025-07-23 13:35:54 -0400

    fix solution

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -7,7 +7,7 @@ resource "azurerm_service_plan" "test" {
 }
 
 resource "azurerm_linux_web_app" "test" {
-  name                = "$AppService-AzureDevops"
+  name                = "AppService-AzureDevops"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   service_plan_id     = azurerm_service_plan.test.id
