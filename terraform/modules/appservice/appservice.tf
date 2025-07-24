12644b4965a98a0923bc568942d930dbbc726fc4
mkeresty <kerestymason@gmail.com>
2025-07-24 13:41:01 -0400

    windows vm not linux bc its .net

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -1,16 +1,18 @@
-resource "azurerm_service_plan" "test" {
+resource "azurerm_app_service_plan" "test" {
   name                = "service-plan"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  os_type             = "Linux"
-  sku_name            = "F1"
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
 }
 
-resource "azurerm_linux_web_app" "test" {
+resource "azurerm_app_service" "test" {
   name                = "${var.host_name}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  service_plan_id     = azurerm_service_plan.test.id
+  app_service_plan_id     = azurerm_service_plan.test.id
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = 0
