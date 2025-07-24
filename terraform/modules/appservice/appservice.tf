55c9e84e73006a29496dbf71a44658b13fe8d1eb
mkeresty <kerestymason@gmail.com>
2025-07-24 14:12:38 -0400

    PLEASE

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -8,16 +8,19 @@ resource "azurerm_app_service_plan" "test" {
   }
 }
 
-resource "azurerm_app_service" "test" {
-  name                = "${var.host_name}"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group}"
-  app_service_plan_id     = azurerm_app_service_plan.test.id
+resource "azurerm_windows_web_app" "test" {
+  name                = var.host_name
+  location            = var.location
+  resource_group_name = var.resource_group
+  service_plan_id     = azurerm_app_service_plan.test.id
+
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = "1"
   }
-  site_config {
+
+    site_config {
     always_on = false
   }
+
 }
