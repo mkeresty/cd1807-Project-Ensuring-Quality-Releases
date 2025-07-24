ce48043bbd3992e79fda8bbaaeed079d0958afe4
mkeresty <kerestymason@gmail.com>
2025-07-24 14:05:28 -0400

    change to post

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -12,7 +12,7 @@ resource "azurerm_app_service" "test" {
   name                = "${var.host_name}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  app_service_plan_id     = azurerm_service_plan.test.id
+  app_service_plan_id     = azurerm_app_service_plan.test.id
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = 0
