b77491b76386da7361067281cdcb8b8897b419b4
mkeresty <kerestymason@gmail.com>
2025-08-10 12:25:52 -0400

    use newer version

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -1,18 +1,16 @@
-resource "azurerm_app_service_plan" "test" {
+resource "azurerm_service_plan" "test" {
   name                = "service-plan"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group}"
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  os_type             = "Windows"
+  sku_name            = "S1" 
 }
 
 resource "azurerm_windows_web_app" "test" {
   name                = var.host_name
   location            = var.location
   resource_group_name = var.resource_group
-  service_plan_id     = azurerm_app_service_plan.test.id
+  service_plan_id     = azurerm_service_plan.test.id
 
 
   app_settings = {
