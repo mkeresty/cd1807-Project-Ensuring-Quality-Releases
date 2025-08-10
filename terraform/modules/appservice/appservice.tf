a28d44ba25aabe451a5eab22fff3652891a8ddee
mkeresty <kerestymason@gmail.com>
2025-08-10 12:15:18 -0400

    basic tier B1

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -3,8 +3,8 @@ resource "azurerm_app_service_plan" "test" {
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   sku {
-    tier = "Free"
-    size = "F1"
+    tier = "Basic"
+    size = "B1"
   }
 }
 
